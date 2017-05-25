3a6c639fd77c9fb42961d4bccf16d8de3b8d9bee
Nooobody <thesilentcoder@outlook.com>
2017-05-25 09:47:40 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PlayAreaComponent } from './play-area/play-area.component';
+import { BlockComponent } from './block/block.component';
+import { PlatformComponent } from './platform/platform.component';
+import { BallComponent } from './ball/ball.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayAreaComponent,
+    BlockComponent,
+    PlatformComponent,
+    BallComponent
   ],
   imports: [
     BrowserModule,
